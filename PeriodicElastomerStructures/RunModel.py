e5b02f08c6174645350f38834713e3b68e8a8ff6
Matheus Fernandes <fernandes@holynx01.rc.fas.harvard.edu>
2017-02-13 17:51:56 -0500

    body with abitrary hole

--- PeriodicElastomerStructures/RunModel.py
+++ PeriodicElastomerStructures/RunModel.py
@@ -19,3 +19,19 @@ import time
 session.journalOptions.setValues(replayGeometry=COORDINATE, recoverGeometry=COORDINATE)
 
 execfile('Functions.py')
+
+mdb.models['Model-1'].ConstrainedSketch(name='__profile__', sheetSize=200.0)
+mdb.models['Model-1'].sketches['__profile__'].rectangle(point1=(-30.0, 26.25), 
+    point2=(26.25, -22.5))
+mdb.models['Model-1'].Part(dimensionality=TWO_D_PLANAR, name='Part-1', type=
+    DEFORMABLE_BODY)
+mdb.models['Model-1'].sketches['__profile__'].Spline(points=((-7.5, 2.5), (-3.75, 
+    7.5), (5.0, 6.25), (7.5, -1.25), (3.75, -6.25), (-1.25, -7.5), (-6.25, 
+    -5.0), (-7.5, 2.5)))
+
+
+mdb.models['Model-1'].parts['Part-1'].BaseShell(sketch=
+    mdb.models['Model-1'].sketches['__profile__'])
+
+
+del mdb.models['Model-1'].sketches['__profile__']

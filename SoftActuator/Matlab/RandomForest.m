45f84ea02070e50868713e86bd28dd3c305b2201
Matheus C. Fernandes <matheuscfernandes@gmail.com>
2017-04-05 15:52:29 -0400

    trained and working matlab code

--- SoftActuator/Matlab/RandomForest.m
+++ SoftActuator/Matlab/RandomForest.m
@@ -13,44 +13,47 @@ DataIn = load('-mat', fileName);
 XP=DataIn.dataPAll(:,1:3);
 YP=DataIn.dataPAll(:,4);
 
-
-Mdl = TreeBagger(10000,XP,YP,'method','regression');
+Mdl = TreeBagger(5000,XP,YP,'method','regression','OOBPredictorImportance','on',... 
+                 'MinLeafSize',1,'NumPrint',100,'NumPredictorsToSample','all');
 
 %% Testing with training data to evaluate method
 HMaster=10;TMaster=1;VNorm=1256.66662598;
 
-H=(77/50)*HMaster/100;T=(86/50)*TMaster/100;
+H=(77/50);T=(86/50);
 
 TestData=importdata('../Data2/H77/W100H77T86-PV.out');
 TestData(:,1)=(TestData(:,1)-min(TestData(:,1)))/VNorm;
 
+
 XPredict=zeros(200,3);
-XPredict(:,1)=linspace(0,12000/VNorm,200)*0+H;
-XPredict(:,2)=linspace(0,12000/VNorm,200)*0+T;
-XPredict(:,3)=linspace(0,12000/VNorm,200);
+XPredict(:,1)=linspace(0,13000/VNorm,200)*0+H;
+XPredict(:,2)=linspace(0,13000/VNorm,200)*0+T;
+XPredict(:,3)=linspace(0,13000/VNorm,200);
 
 figure()
 YPredict=predict(Mdl,XPredict);
 hold on
 plot(XPredict(:,3),YPredict,'b','linewidth',4)
 plot(TestData(:,1),TestData(:,2),'--r','linewidth',4)
+legend('Prediction','Data','location','southeast')
 
 
 %% Testing with never seen data
-
 % HMaster=10;TMaster=1;
 % 
-% H=(75/50)*HMaster/100;T=(300/50)*TMaster/100;
+% H=(75/50);T=(300/50);
 % 
 % TestData=importdata('../pv-H75T300.txt');
+% TestData(:,1)=(TestData(:,1)-min(TestData(:,1)))/VNorm;
 % 
 % XPredict=zeros(200,3);
-% XPredict(:,1)=linspace(0,12000,200)*0+H;
-% XPredict(:,2)=linspace(0,12000,200)*0+T;
-% XPredict(:,3)=linspace(0,12000,200);
+% XPredict(:,1)=linspace(0,12000/VNorm,200)*0+H;
+% XPredict(:,2)=linspace(0,12000/VNorm,200)*0+T;
+% XPredict(:,3)=linspace(0,12000/VNorm,200);
 % 
 % figure()
 % YPredict=predict(Mdl,XPredict);
 % hold on
 % plot(XPredict(:,3),YPredict,'b','linewidth',4)
 % plot(TestData(:,1),TestData(:,2),'--r','linewidth',4)
+% legend('Prediction','Data','location','southeast')

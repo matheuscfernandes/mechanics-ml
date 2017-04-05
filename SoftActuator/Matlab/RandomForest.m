189f5e4af773237f839f05934b21d55e90a3d8a5
Matheus C. Fernandes <matheuscfernandes@gmail.com>
2017-04-04 23:58:58 -0400

    implementing method verification in matlab

--- SoftActuator/Matlab/RandomForest.m
+++ SoftActuator/Matlab/RandomForest.m
@@ -1,7 +1,11 @@
-clc
-close all
-clear all
+%Reference: https://www.mathworks.com/help/stats/treebagger.html
 
+% clc
+close all
+% clear all
+
+
+%% Importing data and learning algorithm
 fileName='MatlabData.mat';
 
 %Importing the data from Python
@@ -10,22 +14,25 @@ XP=DataIn.dataPAll(:,1:3);
 YP=DataIn.dataPAll(:,4);
 
 
+% Mdl = TreeBagger(1000,XP,YP,'Method','regression');
 
-Mdl = TreeBagger(1000,XP,YP,'Method','regression');
+%% Testing with training data to evaluate method
+
+
+%% Testing with never seen data
 
 HMaster=10;TMaster=1;
+
 H=(75/50)*HMaster/100;T=(300/50)*TMaster/100;
 
 TestData=importdata('../pv-H75T300.txt');
+
 XPredict=zeros(200,3);
-XPredict(:,1)=linspace(0,1200,200)*0+H;
-XPredict(:,2)=linspace(0,1200,200)*0+T;
-XPredict(:,3)=linspace(0,1200,200);
+XPredict(:,1)=linspace(0,12000,200)*0+H;
+XPredict(:,2)=linspace(0,12000,200)*0+T;
+XPredict(:,3)=linspace(0,12000,200);
 
 YPredict=predict(Mdl,XPredict);
-
-plot(TestData(:,1),TestData(:,2))
-plot(XPredict(:,3),YPredict)
-
-
-% https://www.mathworks.com/help/stats/treebagger.html
+hold on
+plot(XPredict(:,3),YPredict,'b','linewidth',4)
+plot(TestData(:,1),TestData(:,2),'--r','linewidth',4)
